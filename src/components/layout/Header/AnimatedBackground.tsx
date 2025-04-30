fa8567f157aec2cae79b17ce1dfeb3e50c368bdc
Mostafa266 <mostafa.abdo.ch12@gmail.com>
2025-04-30 12:59:20 +0300

    Add Animation to the Header

--- src/components/layout/Header/AnimatedBackground.tsx
+++ src/components/layout/Header/AnimatedBackground.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function AnimatedBg() {
+  const [isAnimated, setIsAnimated] = useState(false);
+
+  useEffect(() => {
+    setIsAnimated(true);
+  }, []);
   return (
     <>
       <div
-        className={`hidden md:block absolute left-0 bottom-0 w-[48%] bg-gainsboro z-10 overflow-hidden transition-all duration-1000`}
-        style={{ height: "85%" }}
+        className={`absolute left-0 bottom-0 w-[48%] bg-gainsboro z-10 transition-all duration-1000 ${
+          isAnimated ? "h-[85%]" : "h-0"
+        }`}
       ></div>
       <div
-        className={`hidden md:block absolute right-0 bottom-[-10rem] w-[45%] bg-gainsboro z-10 overflow-hidden transition-all duration-1000 delay-100`}
-        style={{ height: "85%" }}
+        className={`absolute right-0 bottom-[-10rem] w-[45%] bg-gainsboro z-10 transition-all duration-1000 delay-100 ${
+          isAnimated ? "h-[85%]" : "h-0"
+        }`}
       ></div>
     </>
   );
